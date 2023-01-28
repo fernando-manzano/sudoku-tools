898a1267b736c40d6016c3ab9c39325e4a278cbe
Fernando Manzano <tola.ware@yahoo.com.ar>
2023-01-28 00:10:25 -0300

    Adding Sudoku Solver for 1x1 and 2x2 boards

--- Sources/SudokuTools/SudokuSolver.swift
+++ Sources/SudokuTools/SudokuSolver.swift
@@ -9,8 +9,56 @@ import Foundation
 
 public class SudokuSolver {
     
-    public func solve(_ board: [[Int?]]) -> [[Int]] {
-        return [[1]]
+    typealias Board = [[Int?]]
+    
+    public func solve(_ board: [[Int?]]) throws -> [[Int]] {
+        var board = board
+        let solutionFound = recursiveSolver(&board)
+        if solutionFound {
+            return createSolution(from: board)
+        } else {
+            throw SudokuSolverError.noSolution
+        }
     }
     
+    private func recursiveSolver(_ board: inout Board) -> Bool {
+        for (rowIndex, row) in board.enumerated() {
+            for (columnIndex, element) in row.enumerated() {
+                if element == nil {
+                    var candidates = findCandidates(board, row: rowIndex, column: columnIndex)
+                    var solutionFound = false
+                    while !solutionFound && !candidates.isEmpty {
+                        board[rowIndex][columnIndex] = candidates.removeFirst()
+                        solutionFound = recursiveSolver(&board)
+                    }
+                    return solutionFound
+                }
+            }
+        }
+        return true
+    }
+    
+    private func createSolution(from board: Board) -> [[Int]] {
+        return board.map { row in
+            row.compactMap { $0 }
+        }
+    }
+    
+    private func findCandidates(_ board: Board, row: Int, column: Int) -> [Int] {
+        guard board[row][column] == nil else {
+            return []
+        }
+        let rowValues = board[row].compactMap { $0 }
+        let columnValues = board.compactMap { row in row[column] }
+        let allValues = 1...board.count
+        let candidates = allValues.filter { value in
+            !columnValues.contains(value) && !rowValues.contains(value)
+        }
+        return candidates
+    }
+    
+}
+
+public enum SudokuSolverError: Error {
+    case noSolution
 }
--- Tests/SudokuToolsTests/SudokuSolverTests.swift
+++ Tests/SudokuToolsTests/SudokuSolverTests.swift
@@ -21,13 +21,89 @@ class SudokuSolverTests: QuickSpec {
                 solver = SudokuSolver()
             }
             
+            context("non solvable board") {
+                describe("of 2x2") {
+                    it("should return error") {
+                        let board = [[1, nil],
+                                     [1, nil]]
+                        expect { try solver.solve(board) }
+                            .to(throwError(SudokuSolverError.noSolution))
+                    }
+                }
+            }
+            
             context("valid board") {
-                describe("empty board of 1x1") {
+                describe("of 1x1") {
                     it("should solve to 1") {
-                        let solution = solver.solve([[nil]])
+                        let solution = try solver.solve([[nil]])
                         expect(solution).to(equal([[1]]))
                     }
                 }
+                
+                describe("of 2x2") {
+                    it("should have solution") {
+                        let board = [[1, nil], [2, nil]]
+                        let expectedSolution = [[1, 2], [2, 1]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    it("should have solution") {
+                        let board = [[1, nil], [nil, 1]]
+                        let expectedSolution = [[1, 2], [2, 1]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    it("should have solution") {
+                        let board = [[1, 2], [nil, nil]]
+                        let expectedSolution = [[1, 2], [2, 1]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    it("should have solution") {
+                        let board = [[nil, nil], [2, 1]]
+                        let expectedSolution = [[1, 2], [2, 1]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    it("should have solution") {
+                        let board = [[nil, 2], [nil, 1]]
+                        let expectedSolution = [[1, 2], [2, 1]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    it("should have solution") {
+                        let board = [[1, nil], [2, nil]]
+                        let expectedSolution = [[1, 2], [2, 1]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    it("should have solution") {
+                        let board = [[2, nil], [nil, 2]]
+                        let expectedSolution = [[2, 1], [1, 2]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    it("should have solution") {
+                        let board = [[2, 1], [nil, nil]]
+                        let expectedSolution = [[2, 1], [1, 2]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    it("should have solution") {
+                        let board = [[nil, nil], [1, 2]]
+                        let expectedSolution = [[2, 1], [1, 2]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    it("should have solution") {
+                        let board = [[nil, 1], [nil, 2]]
+                        let expectedSolution = [[2, 1], [1, 2]]
+                        let solution = try solver.solve(board)
+                        expect(solution).to(equal(expectedSolution))
+                    }
+                    
+                }
+                
             }
 
         }
